2a00808b877f28c67c6587b3efba0153be7c7e90
AyushhNK <neupaneayush3@gmail.com>
2025-08-04 17:33:02 +0545

    chatmessage model

--- WorldWhisper/Data/AppDbContext.cs
+++ WorldWhisper/Data/AppDbContext.cs
@@ -7,5 +7,7 @@ namespace WorldWhisper.Data
     public class AppDbContext:IdentityDbContext<ApplicationUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<ChatMessage> ChatMessages { get; set; }
     }
 }
